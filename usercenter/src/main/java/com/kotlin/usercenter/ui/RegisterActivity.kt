9888780eb9e9b29ec806201754a70849042df5cf
dufeihu <980517916@qq.com>
2019-08-22 20:30:09 +0800

    second

--- usercenter/src/main/java/com/kotlin/usercenter/ui/RegisterActivity.kt
+++ usercenter/src/main/java/com/kotlin/usercenter/ui/RegisterActivity.kt
@@ -2,12 +2,17 @@ package com.kotlin.usercenter.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.kotlin.usercenter.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        register.setOnClickListener {
+            Toast.makeText(this, "register", Toast.LENGTH_LONG).show()
+        }
     }
 }
